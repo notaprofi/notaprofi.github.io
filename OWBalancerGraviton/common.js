b24709a72be9b4ca979b630e6471c368907b0238
notaprofi <jufrkide@mail.ru>
2020-01-22 13:00:05 +0300

    now ResetGamesPlayed changes update time only when checkin > 0

--- OWBalancerGraviton/common.js
+++ OWBalancerGraviton/common.js
@@ -654,19 +654,23 @@ function unmark_players() {
 function reset_players_marks() {
 	if(confirm("This will reset number of games played record for all users of the spreadsheet. Are you sure?")) {
 		for( i in lobby ) {
-			lobby[i].mark = false;
-			lobby[i].games_checkedin = 0;
-			lobby[i].games_played = 0;
-			lobby[i].last_updated = new Date();
+			if (lobby[i].games_checkedin > 0) {
+				lobby[i].mark = false;
+				lobby[i].games_checkedin = 0;
+				lobby[i].games_played = 0;
+				lobby[i].last_updated = new Date();
+			}
 		}
 		if ( is_role_lock_enabled() ) 	{
 			for( let team of  [team1_slots, team2_slots]) {
 				for (class_name in team) {
 					for( i in team[class_name] ) {
-						team[class_name][i].mark = false;
-						team[class_name][i].games_checkedin = 0;
-						team[class_name][i].games_played = 0;
-						team[class_name][i].last_updated = new Date();
+						if (team[class_name][i].games_checkedin > 0) {
+							team[class_name][i].mark = false;
+							team[class_name][i].games_checkedin = 0;
+							team[class_name][i].games_played = 0;
+							team[class_name][i].last_updated = new Date();
+						}
 					}
 				}
 			}
